30cf9fcf347bf9505e1ae4e0eea220c3e0697828
RichardYang <zhongtian.yang@qq.com>
2020-08-28 09:04:37 +0800

    [add] add grade

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,17 +1,18 @@
 $(function () {
     var config = {
         "直升初一": "",
-        "常规初一": "",
-        "直升初二": "",
-        "常规初二": "",
-        "初三": "",
+        "常规初一(张潇文)": "https://www.bilibili.com/blackboard/player.html?aid=329422249&bvid=BV1CA411n7tb&cid=229439539",
+        "常规初一(陈纹珊)": "https://www.bilibili.com/blackboard/player.html?aid=926907683&bvid=BV1BT4y1L7nn&cid=229438759",
+        "直升初二": "https://www.bilibili.com/blackboard/player.html?aid=244474076&bvid=BV1Hv411177f&cid=229623025",
+        "常规初二": "https://www.bilibili.com/blackboard/player.html?aid=884413805&bvid=BV1pK4y1e7vw&cid=229321093",
+        "初三": "https://www.bilibili.com/blackboard/player.html?aid=839454822&bvid=BV1654y1276i&cid=229323852",
         "直升高一": "",
-        "高一": "",
-        "高二": "",
-        "高三": "",
+        "高一": "https://www.bilibili.com/blackboard/player.html?aid=669413833&bvid=BV1da4y1773P&cid=229324509",
+        "高二": "https://www.bilibili.com/blackboard/player.html?aid=669411382&bvid=BV19a4y177n8&cid=229325495",
+        "高三": "https://www.bilibili.com/blackboard/player.html?aid=626952278&bvid=BV1et4y1S7Yx&cid=229326416",
         "国际部高一": "",
-        "国际部高二": "",
-        "国际部高三": "https://v.qq.com/txp/iframe/player.html?vid=m3141zuty0z"
+        "国际部高二": "https://rrx.cn/view-wwq3yq",
+        "国际部高三": ""
     }
     var loadData = function () {
         $(".v-bind").each(function () {
